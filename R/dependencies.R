fec780f8d0d238b764cdf6fbb74e52397c5cb7f1
Mauro Masiero <mauromiguelm@hotmail.com>
2024-02-06 11:15:52 +0100

    simplify dependencies for dbg

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,19 +1,13 @@
 #' Load scripts necessary for the package.
 #'
 #' @description This function loads the necessary scripts for the package.
-#'
 #' @return A list of scripts.
-load_script <- function(){
+#' @keywords internal
+load_wizard <- function(){
   htmltools::htmlDependency(
-    name = "bootstrap",
-    version = "v5.0.2",
+    name = "wizard",
+    version = "5.0.2",
     src = "assets",
-    script = "/bootstrap.min.js"
-  )
-  htmltools::htmlDependency(
-    name = "Wizard-JS",
-    version = "v1.9.9",
-    src = "assets",
-    script = "/bootstrap.min.js"
+    script = "wizard.min.js"
   )
 }
