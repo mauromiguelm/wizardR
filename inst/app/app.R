5cfc2fb4fea56904d0eef37cea440a48bdc74c69
Mauro Masiero <mauromiguelm@hotmail.com>
2024-02-07 14:44:15 +0100

    add a plot output and pass data-orientation argument

--- inst/app/app.R
+++ inst/app/app.R
@@ -2,19 +2,28 @@ library(shiny)
 library(wizardR)
 
 ui <- fluidPage(
-  "wizard",
+  "wizardR demo",
   theme = bslib::bs_theme(version = 5L),
     wizard(
+      orientation = "horizontal",
       wizard_step(
         h5("hello")
       ),
       wizard_step(
         h5("world")
+      ),
+      wizard_step(
+        plotOutput("plot")
       )
     )
 )
 
-server <- \(input, output, session) {
+server <- function(input, output, session) {
+  
+  output$plot <- renderPlot({
+    plot(cars)
+  })
+
 }
 
 shinyApp(ui, server)
