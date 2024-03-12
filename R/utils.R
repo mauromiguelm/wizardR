1f1608cc355b682936ec44bfd749e849a45ebfed
cpsievert <cpsievert@users.noreply.github.com>
2024-03-12 16:06:53 +0000

    Style code (GHA)

--- R/utils.R
+++ R/utils.R
@@ -1,27 +1,25 @@
 #' @title Lock wizard
-#' 
+#'
 #' Server-side function to lock the wizard.
-#' 
+#'
 #' @param id wizard id
 #' @param session shiny session
 #' @export
 lock <- function(
-    id, 
-    session = shiny::getDefaultReactiveDomain()
-    ){
-    session$sendInputMessage(id, list(type = "lock"))
+    id,
+    session = shiny::getDefaultReactiveDomain()) {
+  session$sendInputMessage(id, list(type = "lock"))
 }
 
 #' @title Unlock wizard
-#' 
+#'
 #' Server-side function to unlock the wizard.
-#' 
+#'
 #' @param id wizard id
 #' @param session shiny session
 #' @export
 unlock <- function(
     id,
-    session = shiny::getDefaultReactiveDomain()
-    ){
-    session$sendInputMessage(id, list(type = "unlock"))
+    session = shiny::getDefaultReactiveDomain()) {
+  session$sendInputMessage(id, list(type = "unlock"))
 }
