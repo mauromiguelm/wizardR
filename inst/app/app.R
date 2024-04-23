2af4fad7669e7fef9fb36874d6e366a53a2a9840
Mauro Masiero <mauromiguelm@hotmail.com>
2024-04-23 13:41:43 +0200

    add step_ids in app

--- inst/app/app.R
+++ inst/app/app.R
@@ -12,18 +12,22 @@ ui <- fluidPage(
       # start sequence of steps
       wizard_step(
         step_title = "Hello tag",
+        step_id ="step-helo",
         shiny::h5("hello, this is step 0.")
       ),
       wizard_step(
         step_title = "Numeric input",
+        step_id ="step-input",
         shiny::numericInput("number", "Select a number", value = 30, min = 20, max = 100)
       ),
       wizard_step(
         step_title = "Plot output from input",
+        step_id = "step-output",
         plotOutput("plot")
       ),
       wizard_step(
-        shiny::h5("No step title defined. This is the last step.")
+        shiny::h5("No step title defined. This is the last step."),
+        step_id = "step-last"
       )
     )
 )
@@ -41,6 +45,10 @@ server <- function(input, output, session) {
   # show the wizard
   observeEvent(input$show_wizard, {
     wizard_show("my_modal")
+  })
+
+  observeEvent(input$my_modal, {
+    print(input$my_modal)
   })
 }
 
